7a9faf492c203d4432f0464990ef4ca8d6235306
Sandro Roberto Bezerra Filho <sandrorbf687@gmail.com>
2018-03-12 15:23:52 -0300

    Update Roteiro2-1.c

--- Roteiro2-1.c
+++ Roteiro2-1.c
@@ -3,28 +3,43 @@
 #include <string.h>
 #include <math.h>
 
-//Para o array gAlunos[100] do tipo tAluno ser global, a struct tambem devera ser global.
+
  typedef struct{
-        char nome [15];
-        char endereco [15];
-        char matricula [15];
-        char curso [15];
+        char nome [30];
+        char endereco [50];
+        char matricula [10];
+        char curso [30];
     } tAluno;
-tAluno Registro;
 
     tAluno gAlunos[100];
 
-unsigned int consultaAlunosPorCurso(char*curso){
+unsigned int consultaAlunosPorCurso(){
+    int i, t = 0;
+    char curso[30];
+
+    printf("Digite o curso a ser procurado.\n");
+    scanf("%s", &curso[30]);
+    for (i = 0; i <= 100; i++){
+        if (curso[30] == gAlunos[i].curso[30]){
+            t++;
+        }
+    }
+    printf("%d alunos estao matriculados no curso procurado.\n", t);
 }
 
 int main(void){
 
     int i;
 
-    for(i = 0; i <= 2; i++){
+    for(i = 0; i < 2; i++){
         printf("Registro de alunos. Digite o nome do aluno %d de Computacao.\n", i+1);
-        getchar();
-        gets(gAlunos[i].nome[15]);
+        scanf("%s", &gAlunos[i].nome[30]);
+        //printf("%s\n", gAlunos[i].nome[30]);
+        gAlunos[i].curso[30] = "Computacao";
     }
+    printf("Registro de alunos. Digite o nome do aluno %d de Fisica.\n", 1);
+    scanf ("%s", &gAlunos[2].nome[30]);
+
+    consultaAlunosPorCurso();
 return 0;
 }

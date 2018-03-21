d6b6e2fa88a7d96de89e82c6c0780d1ec220054a
Sandro Roberto Bezerra Filho <sandrorbf687@gmail.com>
2018-03-21 10:03:13 -0300

    Update Roteiro2-2.c

--- Roteiro2-2.c
+++ Roteiro2-2.c
@@ -36,9 +36,10 @@ void nomebanda();
 
       int ranking_teclado=0;
       register int y=0;
-      printf("\n DIGITE UM NUMERO NO RANKING DE 1 A 5 : \n"); scanf("%d", &ranking_teclado);
+      printf("\nInsira o numero de ranking. 1 A 5 : \n"); 
+      scanf("%d", &ranking_teclado);
 
-        for(y=0;y<5;y++){
+        for(y = 0; y < 5; y++){
             if(vetBandas[y].ranking == ranking_teclado){
                 printf("Registro %d:\n", y+1);
                 printf("\nNome: %s\n", vetBandas[y].nome);
@@ -54,81 +55,74 @@ void nomebanda();
         char estilo_teclado[10];
         register int y=0;
 
-        printf("\n DIGITE UM ESTILO DE MUSICA: \n"); scanf("%s", &estilo_teclado);
+        printf("\n Insira o estilo de musica.: \n"); 
+        scanf("%s", &estilo_teclado);
 
-        for(y=0;y<5;y++){
+        for(y = 0; y < 5; y++){
             if(strcmp(vetBandas[y].estilo, estilo_teclado)==0){
                 printf("Registro %d:\n", y+1);
                 printf("\nNome: %s\n", vetBandas[y].nome);
                 printf("\estilo: %s\n", vetBandas[y].estilo);
                 printf("\nNumero de integrantes: %d\n", vetBandas[y].numero_integrantes);
-                    printf("\Ranking: %d\n", vetBandas[y].ranking);
-
+                printf("\Ranking: %d\n", vetBandas[y].ranking);
             }
         }
-
-
     }
-
-
     void nomebanda(){
         char nomebanda[10];
-        register int y=0;
-        int indicador=0;
+        register int y = 0;
+        int indicador = 0;
 
-        printf("\n DIGITE UM NOME DA BANDA AQUAL DESEJA CONSULTAR: \n"); scanf("%s", &nomebanda);
+        printf("\n Insira a banda a ser consultada: \n"); 
+        scanf("%s", &nomebanda);
 
-        for(y=0;y<5;y++){
+        for(y = 0; y < 5; y++){
             if(strcmp(vetBandas[y].nome, nomebanda)==0){
-            printf("ESSA BANDA ESTA ENTRE SUAS FAVORITAS\n");
-                indicador=1;
+            printf("A banda digitada esta entre uma de suas favoritas.\n");
+                indicador = 1;
             }
         }
-
-        if (indicador==0){
-            printf("ESSA BANDA NAO ESTA ENTRE SUAS FAVORITAS\n");
+        if (indicador == 0){
+            printf("A banda digitada nao esta entre uma de suas favoritas.\n");
         }
-
-
     }
-
-
     void preencherBanda(){
-        int x=0;
-
-        for(x=0;x<5;x++){
+        int x = 0;
+        
+        for(x = 0; x < 5; x++){
             printf("Registro %d:", x+1);
-            printf("\nNome: ");  scanf("%s", &vetBandas[x].nome);
-            printf("\estilo: ");  scanf("%s", &vetBandas[x].estilo);
-            printf("\nNumero de integrantes: ");   scanf("%d", &vetBandas[x].numero_integrantes);
-                printf("\Ranking: ");  scanf("%d", &vetBandas[x].ranking);
+            printf("\nNome: ");  
+            scanf("%s", &vetBandas[x].nome);
+            printf("\nestilo: ");  
+            scanf("%s", &vetBandas[x].estilo);
+            printf("\nNumero de integrantes: ");   
+            scanf("%d", &vetBandas[x].numero_integrantes);
+            printf("\nRanking: ");  
+            scanf("%d", &vetBandas[x].ranking);
         }
     }
-
-
     void printaBanda(){
-        int x=0;
+        int x = 0;
 
-        for(x=0;x<5;x++){
+        for(x = 0; x < 5; x++){
                 printf("Registro %d:\n", x+1);
                 printf("\nNome: %s\n", vetBandas[x].nome);
-                printf("\estilo: %s\n", vetBandas[x].estilo);
+                printf("\nestilo: %s\n", vetBandas[x].estilo);
                 printf("\nNumero de integrantes: %d\n", vetBandas[x].numero_integrantes);
-                printf("\Ranking: %d\n", vetBandas[x].ranking);
+                printf("\nRanking: %d\n", vetBandas[x].ranking);
 
         }
     }
-
     void menu(){
         int escolha=0;
             do{
             printf("\n ------------- MENU ------------- ");
-            printf("\n 1- PREENCHER BANDAS");
-            printf("\n 2- IMPRIMIR BANDAS");
-            printf("\n 3- BUSCA POR RANKING");
-            printf("\n 4- BUSCA POR ESTILO");
-            printf("\n 5- BUSCA POR NOME");
-            printf("\n 0- SAIR\n\n\n");
+            printf("\n 1- Preenchimento de bandas");
+            printf("\n 2- Exibir bandas");
+            printf("\n 3- Pesquisar por ranking");
+            printf("\n 4- Pesquisar por estilo");
+            printf("\n 5- Pesquisar por nome");
+            printf("\n 0- Encerrar o programa\n\n\n");
             scanf("%d", &escolha);
             switch(escolha){
                 case 1: preencherBanda();
